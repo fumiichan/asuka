5029b824876f15312d08c34a58b18dfc1be2162b
Aiko Fujimoto <aikoo_fujimotoo@yahoo.co.jp>
2020-07-08 15:07:55 +0900

    refactor: use StringBuilder to build doujin information to display and write
    This fixes issues where it mixes information of other downloads

--- asuka/Base/DisplayDoujinMetadata.cs
+++ asuka/Base/DisplayDoujinMetadata.cs
@@ -1,6 +1,7 @@
 using System;
 using System.IO;
 using System.Collections.Generic;
+using System.Text;
 using System.Linq;
 using asuka.Model;
 
@@ -14,33 +15,7 @@ namespace asuka.Base
     /// <param name="data">The nhentai response data.</param>
     public static void Display (Response data)
     {
-      Console.WriteLine("Title ================================================");
-      Console.WriteLine("Japanese: " + data.Title.Japanese);
-      Console.WriteLine("English: " + data.Title.English);
-      Console.WriteLine("Pretty: " + data.Title.Pretty);
-      Console.WriteLine("======================================================");
-
-      // Group each tags to display.
-      string[] Artists = TagGrouper(data.Tags, "artist");
-      string[] Parodies = TagGrouper(data.Tags, "parody");
-      string[] Characters = TagGrouper(data.Tags, "character");
-      string[] Tags = TagGrouper(data.Tags, "tag");
-      string[] Categories = TagGrouper(data.Tags, "category");
-      string[] Languages = TagGrouper(data.Tags, "language");
-      string[] Groups = TagGrouper(data.Tags, "group");
-
-      Console.WriteLine("Artist: " + string.Join(", ", Artists));
-      Console.WriteLine("Parodies: " + string.Join(", ", Parodies));
-      Console.WriteLine("Characters: " + string.Join(", ", Characters));
-      Console.WriteLine("======================================================");
-      Console.WriteLine("Tags: " + string.Join(", ", Tags));
-      Console.WriteLine("Categories: " + string.Join(", ", Categories));
-      Console.WriteLine("Languages: " + string.Join(", ", Languages));
-      Console.WriteLine("Groups: " + string.Join(", ", Groups));
-      Console.WriteLine("======================================================");
-      Console.WriteLine("Total Pages: " + data.TotalPages.ToString());
-      Console.WriteLine("URL: https://nhentai.net/g/" + data.Id.ToString());
-      Console.WriteLine("");
+      Console.WriteLine(BuildInfo(data));
     }
 
     /// <summary>
@@ -50,14 +25,8 @@ namespace asuka.Base
     /// <param name="filePath">path to save the info file</param>
     public static void GenerateInfoFile (Response data, string filePath)
     {
-      using var writer = new StreamWriter(filePath);
-      TextWriter oldConsoleOutput = Console.Out;
-
-      Console.SetOut(writer);
-
-      Display(data);
-
-      Console.SetOut(oldConsoleOutput);
+      string info = BuildInfo(data);
+      File.WriteAllText(filePath, info);
     }
 
     /// <summary>
@@ -72,6 +41,41 @@ namespace asuka.Base
       string[] group = Group.Select(value => value.Name).ToArray();
 
       return group;
+    }
+
+    private static string BuildInfo (Response data)
+    {
+      StringBuilder output = new StringBuilder();
+
+      output.AppendLine("Title ================================================");
+      output.AppendLine("Japanese: " + data.Title.Japanese);
+      output.AppendLine("English: " + data.Title.English);
+      output.AppendLine("Pretty: " + data.Title.Pretty);
+      output.AppendLine("======================================================");
+
+      // Group each tags to display.
+      string[] Artists = TagGrouper(data.Tags, "artist");
+      string[] Parodies = TagGrouper(data.Tags, "parody");
+      string[] Characters = TagGrouper(data.Tags, "character");
+      string[] Tags = TagGrouper(data.Tags, "tag");
+      string[] Categories = TagGrouper(data.Tags, "category");
+      string[] Languages = TagGrouper(data.Tags, "language");
+      string[] Groups = TagGrouper(data.Tags, "group");
+
+      output.AppendLine("Artist: " + string.Join(", ", Artists));
+      output.AppendLine("Parodies: " + string.Join(", ", Parodies));
+      output.AppendLine("Characters: " + string.Join(", ", Characters));
+      output.AppendLine("======================================================");
+      output.AppendLine("Tags: " + string.Join(", ", Tags));
+      output.AppendLine("Categories: " + string.Join(", ", Categories));
+      output.AppendLine("Languages: " + string.Join(", ", Languages));
+      output.AppendLine("Groups: " + string.Join(", ", Groups));
+      output.AppendLine("======================================================");
+      output.AppendLine("Total Pages: " + data.TotalPages.ToString());
+      output.AppendLine("URL: https://nhentai.net/g/" + data.Id.ToString());
+      output.AppendLine("");
+
+      return output.ToString();
     }
   }
 }
