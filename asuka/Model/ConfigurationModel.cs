7a1c017936244d25f4c2062801cc3cde50143632
Aiko Fujimoto <aikoo_fujimotoo@yahoo.co.jp>
2020-07-08 19:36:12 +0900

    fix: use uint to prevent negative values on configuration

--- asuka/Model/ConfigurationModel.cs
+++ asuka/Model/ConfigurationModel.cs
@@ -5,10 +5,10 @@ namespace asuka.Model
   public class ConfigurationModel
   {
     [JsonProperty("parallelTasks")]
-    public int ConcurrentTasks { get; set; } = 2;
+    public uint ConcurrentTasks { get; set; } = 2;
 
     [JsonProperty("parallelImageDownload")]
-    public int ConcurrentImageTasks { get; set; } = 2;
+    public uint ConcurrentImageTasks { get; set; } = 2;
 
     [JsonProperty("preferJapanese")]
     public bool PreferJapanese { get; set; } = false;
